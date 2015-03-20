1253da4d317ff3e05db2fc36bd087c8f92dd057f
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-03-20 20:24:53 +0100

    setup.py: hook localization management in 'build' and 'install'.
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- setup.py
+++ setup.py
@@ -22,8 +22,72 @@
 
 import os
 from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.install import install
+
+import subprocess
+import shutil
 
 data_path = "/usr/share/vera-control-center"
+l10n_path = "./po"
+
+class CustomBuild(build):
+	"""
+	Hooks.
+	"""
+	
+	def run(self):
+		"""
+		Runs the installation.
+		"""
+		
+		super().run()
+		
+		# Build mos
+		for directory, dirnames, filenames in os.walk(l10n_path):
+			for file_ in filenames:
+				if file_.endswith(".po"):
+					source = os.path.join(directory, file_)
+					target_dir = os.path.join("./build", directory)
+					target = os.path.join(target_dir, file_.replace(".po",".mo"))
+					
+					if not os.path.exists(target_dir):
+						os.makedirs(target_dir)
+					
+					print("Compiling translation %s" % file_)
+					subprocess.call(["msgfmt", "--output-file=%s" % target, source])
+
+class CustomInstall(install):
+	"""
+	Hooks.
+	"""
+	
+	def run(self):
+		"""
+		Runs the installation.
+		"""
+		
+		super().run()
+		
+		# Install mos
+		for directory, dirnames, filenames in os.walk(os.path.join("./build", l10n_path)):
+			for file_ in filenames:
+				if file_.endswith(".mo"):
+					source = os.path.join(directory, file_)
+					target_dir = os.path.join(
+						self.root if self.root else "/",
+						"usr/share/locale",
+						file_.replace(".mo",""),
+						"LC_MESSAGES"
+					)
+					target = os.path.join(target_dir, os.path.basename(directory) + ".mo")
+					
+					if not os.path.exists(target_dir):
+						os.makedirs(target_dir)
+					
+					shutil.copyfile(source, target)
+					os.chmod(target, 644)
+
 
 def get_module_files():
 	"""
@@ -50,6 +114,10 @@ data_files += [
 ]
 
 setup(
+	cmdclass={
+		"build": CustomBuild,
+		"install": CustomInstall
+	},
 	name='vera-control-center',
 	version='0.34',
 	description='Simple, fast and modular control center for the Vera Desktop Environment',
