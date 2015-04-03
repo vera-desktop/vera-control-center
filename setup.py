6abe862ba15f3cba54c996e4cdaf1cfb63e11d6c
Eugenio Paolantonio (g7) <me@medesimo.eu>
2015-04-03 14:48:17 +0200

    1.0.0
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- setup.py
+++ setup.py
@@ -209,7 +209,7 @@ setup(
 		"install": CustomInstall
 	},
 	name='vera-control-center',
-	version='0.35',
+	version='1.0.0',
 	description='Simple, fast and modular control center for the Vera Desktop Environment',
 	author='Eugenio Paolantonio',
 	author_email='me@medesimo.eu',
